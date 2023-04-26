2c86a088f036fbc4faf96f71660af28f11449c6f
Hamza Kenbouch <hamzakenbouch.co@gmail.com>
2023-04-25 18:03:17 -0700

    Printf Project

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+
+int _putchar(char c)
+void intToChar(int num, char* str)
+void spec_d_i(i, p)
+
+
+#endif
